aa2254f80fb3c4779f67736a2025633a2978d6b7
yuanyuan <1356876882@qq.com>
2018-02-03 20:34:43 +0800

    e3-manager commmit

---
+++ e3-manager/e3-manager-web/src/main/java/cn/e3/manager/controller/ItemController.java
@@ -0,0 +1,29 @@
+package cn.e3.manager.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cn.e3.manager.service.ItemService;
+import cn.e3.pojo.TbItem;
+
+@RestController
+public class ItemController {
+
+	@Autowired
+	private ItemService itemService;
+
+	/**
+	 * 需求:根据商品id查询商品数据 
+	 * 请求:item/{itemId} 
+	 * 参数:Long itemId 
+	 * 返回值:TbItem
+	 */
+	@RequestMapping("item/{itemId}")
+	public TbItem findItemById(@PathVariable Long itemId) {
+		TbItem item = itemService.findItemById(itemId);
+		return item;
+	}
+
+}
